831fa758830a9984d9cf77bd74e501899d6065a2
TahmidRaven <raven.of.the.endless@gmail.com>
2025-01-09 01:07:56 +0600

    first program_newline_endline_tabline_multilinecommentting

--- Level_0/konnichiwa_sekai.cpp
+++ Level_0/konnichiwa_sekai.cpp
@@ -2,6 +2,16 @@
 
 int main(){
 
-    std::cout << "Konnichiwa, Sekai!" << std::endl;
+    std::cout << "Konnichiwa, Sekai!" << std::endl;  // Japanese for "Hello, World!" 
+    std::cout << "Sayonara! Sekai" << std::endl; // std::endl; gives a endline
+
+    std::cout << "Konnichiwa, Sekai!" << '\t'; // tab
+    std::cout << "Konnichiwa, Sekai!" << '\n'; // newline
+/* 
+    std::endl will flush the output buffer;
+    which means that it will write all the output to the console at that point.
+    tho '\n' is kinda better for performance wise. 
+*/
+
     return 0;
 }
