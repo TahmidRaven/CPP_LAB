3239b37d403e1619766db78ac9f4e1e107f10c95
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2025-01-12 03:38:35 +0600

    user_input

---
+++ Level_1/8.user_input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    // user input 
+    // std::cin >> variable_name; 
+    // std::cout << variable_name; 
+
+    int age;
+    std::string name;
+
+    std::cout << "Enter your name: " << "\n";
+    std::cin >> name;
+    std::cout << "Hello There Lord, " << name << "\n";
+    std::cout << "Enter your age: " << "\n";
+    std::cin >> age;
+    std::cout << "O MY Goddo! You are " << age << " years old" << "\n";
+
+    return 0;
+
+
+}
