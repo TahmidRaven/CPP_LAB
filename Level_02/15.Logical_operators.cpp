237846fe8bbc8bcd46be446d862fde66aa0820ad
TahmidRaven <raven.of.the.endless@gmail.com>
2025-01-24 01:31:09 +0600

    logical operators && || !

--- Level_02/15.Logical_operators.cpp
+++ Level_02/15.Logical_operators.cpp
@@ -3,14 +3,23 @@
 int main() {
 
     int temp;
+    bool sunny;
+
     std::cout << "Enter your current temperature: ";
     std::cin >> temp;
 
-    if(temp >0 && temp< 30){
-        std::cout << "the temperature is good." << std::endl;
+    if(temp >1 && temp< 30){
+        std::cout << "the temperature is good\n";
+    }
+    else if (temp >= 30 || temp <=0){
+        std::cout << "the temperature is bad.\n";
+    }
+
+    if(!sunny){
+        std::cout << "It is cloudy outside.\n";               
     }
     else{
-        std::cout << "the temperature is not good." << std::endl;
-    }
+        std::cout << "It is Sunny outside." << std::endl;
+    }   
     return 0;
 }
