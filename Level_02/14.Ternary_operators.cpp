bd89024de40ef2c7a822feaff022a30c093c8f4b
TahmidRaven <raven.of.the.endless@gmail.com>
2025-01-23 18:28:59 +0600

    ternary operations

--- Level_02/14.Ternary_operators.cpp
+++ Level_02/14.Ternary_operators.cpp
@@ -3,8 +3,17 @@
 int main() 
 {
     int grade = 90;
+    int number = 9;
+    bool hungry = false;
 
     grade >= 50 ? std::cout << "Passed\n" : std::cout << "Failed\n";
+
+
+    // number % 2 == 1 ? std:: cout << "odd" : std::cout << "even" << std::endl;
+    std::cout << (number % 2 == 0 ? "Even" : "Odd") << std::endl;
+
+
+    std::cout << (hungry? "I'm hungry" : "I'm not hungry") << std::endl;
 
     return 0;
 }
