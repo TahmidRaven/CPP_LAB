9b4a7b970be163f06a6803698fc9538b32337320
TahmidRaven <raven.of.the.endless@gmail.com>
2025-01-28 22:04:09 +0600

    for loops

---
+++ Level_02/20.for_loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // For loop
+    for (int i = 0; i <= 11; i+=3){
+        std::cout << "i: " << i << std::endl;
+    }
+    std::cout << "Happy c++ learing Bro\n";
+
+    for (int i = 4; i >=0 ; i--){
+        std::cout << "i: " << i << std::endl;
+    }
+
+    return 0;
+}
--- Level_02/tempCodeRunnerFile.cpp
+++ Level_02/tempCodeRunnerFile.cpp
@@ -1,1 +1,5 @@
-should
+    // // For loop
+    // for (int i = 0; i <= 11; i+=3){
+    //     std::cout << "i: " << i << std::endl;
+    // }
+    // std::cout << "Happy c++ learing Bro";
