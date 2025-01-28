d3702ad886431689c7aa13f2fd0da312a0c83fa8
TahmidRaven <raven.of.the.endless@gmail.com>
2025-01-28 21:30:59 +0600

    branching finished

--- Level_02/17.String_methods.cpp
+++ Level_02/17.String_methods.cpp
@@ -6,6 +6,8 @@ int main() {
 
     std::cout << "Enter your name: ";
     std::getline(std::cin >> std::ws, name);   // std::ws removes any leading or trailing whitespace
+    name.clear();
+    name.append("John Doe");
 
     if(name.length() > 12) {
         std::cout << "Name is too long!" << std::endl;
@@ -13,6 +15,28 @@ int main() {
     else {
         std::cout << "Hello, " << name << "!" << std::endl;
     }
+    name.empty() ? std::cout << "Name is empty!" : std::cout << "Name is not empty!" << std::endl;
 
+   
+    std::cout << "Char at 2nd position is!";
+    std::cout << name.at(2) << std::endl;
+
+    name.insert(0, "Mr. ");
+    std::cout << "Updated name: " << name << std::endl;
+
+    std::cout << name.find(" ") << " is the white space position between name" << std::endl;
+    
+    name.erase(9, 11); // remove "Doe"
+    std::cout << "2nd Updated name: " << name << std::endl;
+
+
+    name.insert(9, "Smith");
+    std::cout << "3rd Updated name: " << name << std::endl;
+    
+    // remove "Smith: with "Wick"
+    name.replace(9, 5, "Wick");
+    std::cout << "4th Updated name: " << name << std::endl;
+        
     return 0;
+
 }
