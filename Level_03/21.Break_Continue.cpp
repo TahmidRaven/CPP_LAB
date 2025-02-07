090fa9823ec22d69126221d0922dd75488949bd0
TahmidRaven <raven.of.the.endless@gmail.com>
2025-02-08 01:55:23 +0600

    break * continue

---
+++ Level_03/21.Break_Continue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+        // break = break our of a loop and
+        // continue = skip the current iteration and move to the next one
+
+        for(int i = 1; i <-20; i++){
+            if(i == 13){
+
+                break;
+            
+            }
+            std::cout << i << "\n";
+        }
+
+    return 0;
+
+}
