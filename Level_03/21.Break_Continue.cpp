ce58aeeeb496ce18b61a53cf7a011e29992247d8
TahmidRaven <raven.of.the.endless@gmail.com>
2025-02-08 02:01:24 +0600

    fixed code

--- Level_03/21.Break_Continue.cpp
+++ Level_03/21.Break_Continue.cpp
@@ -5,7 +5,7 @@ int main(){
         // break = break our of a loop and
         // continue = skip the current iteration and move to the next one
 
-        for(int i = 1; i <-20; i++){
+        for(int i = 1; i <=20; i++){
             if(i == 13){
 
                 break;
